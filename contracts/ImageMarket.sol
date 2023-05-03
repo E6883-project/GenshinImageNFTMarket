01f9e88fff7003b076ef16625b80fe2672ad16d0
Jingwei Zhang <daniel@JingweideMacBook-Pro.local>
2023-05-03 20:20:05 +0200

    Add withdraw

--- contracts/ImageMarket.sol
+++ contracts/ImageMarket.sol
@@ -8,22 +8,46 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 contract  Marketplace is ReentrancyGuard {
     struct GenshinImage {
         uint256 image_id;
+        uint256 token_id;
         IERC721 nft;
+        address payable seller;
+        bool sold;
 
     }
+
+    mapping(uint256 => GenshinImage) public items;
+    mapping(uint => bool) public withdraw_list;
+
 
     function createNFT() internal pure returns (string memory) {
         return '';
     }
 
 
-    function transferNFT () {}
+    function transferNFT (uint256 image_id) external payable nonReentrant{
 
-    function listNFTForSale () {}
+    }
+
+    function listNFTForSale (uint256 image_id) external payable nonReentrant{
+
+    }
 
 
-    function removeNFTFromSale
+    function removeNFTFromSale (uint256 _image_id) external payable nonReentrant{
+       require(_image_id >= 0, "Image ID not valid");
+       require(items[_image_id].seller == msg.sender);
+       require(items[_image_id].sold == false);
 
-    function purchaseNFT
+       GenshinImage storage temp = items[_image_id];
+
+       temp.nft.transferFrom(address(this), msg.sender, temp.token_id);
+       withdraw_list[_image_id] = true;
+
+       
+    }
+
+    function purchaseNFT(uint256 image_id) external payable nonReentrant{
+
+    }
 
 }
