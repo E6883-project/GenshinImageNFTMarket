1dbadb7c3d6f97c66f1c9ff3ad1324e44b661a28
Zhengping Zhu <zhuzhengping@zhuzhenngdembp2.mynetworksettings.com>
2023-05-03 23:25:32 -0400

    functions done

--- contracts/ImageMarket.sol
+++ contracts/ImageMarket.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
@@ -16,7 +17,7 @@ contract  Marketplace is ReentrancyGuard {
         uint256 price;
         address payable seller;
         bool sold;
-        // unint256 most_sold;
+        uint256 most_sold;
         string image_url;
     }
 
@@ -115,6 +116,7 @@ contract  Marketplace is ReentrancyGuard {
             _price,
             payable(msg.sender),
             false,
+            new_most_sold,
             _image_url
         );
         originalmostsold[itemCount] = _most_sold_num;
@@ -122,7 +124,7 @@ contract  Marketplace is ReentrancyGuard {
 
     }
     
-    function listNFTForSale(IERC721 _nft, uint256 _tokenId, uint256 _price, string memory _image_url) external nonReentrant {
+    function recreateNFT(IERC721 _nft, uint256 _tokenId, uint256 _price, string memory _image_url) external nonReentrant {
         require(_price > 0, "The must should be positive.");
         require(appear_or_not[_image_url] == true, "The image has not been created yet.");
         require(owner[_image_url] == msg.sender, "This sender does not own this image.");
@@ -143,24 +145,108 @@ contract  Marketplace is ReentrancyGuard {
             _price,
             payable(msg.sender),
             false,
+            new_most_sold,
             _image_url
         );
         emit ImageCreated(itemCount, address(_nft), _tokenId, _price, msg.sender, new_most_sold, _image_url);
 
     }
 
-    // function createNFT() internal pure returns (string memory) {
-    //     return '';
-    // }
+    function listNFTForSale(uint256 _image_id, uint256 _price) external nonReentrant {
+        require(_image_id > 0 && _image_id < itemCount, "Image not found.");
+        require(buyerstorage[_image_id] == msg.sender, "The image is not owned by the sender");
+        require(_price > 0, "Price must be greater than 0");
+        //Get the information of NFT.
+        GenshinImage storage image = items[_image_id];
+        image.seller = payable(msg.sender);
+        image.sold = false;
+        image.price = _price;
+        //After writing the new information of image, transfer the NFT to market
+        image.nft.transferFrom(msg.sender, address(this), image.token_id);
+        //Store the new NFT
+        items[_image_id] = image;
+        buyerstorage[_image_id] = address(123);
+        withdrawornot[_image_id] = false;
+    }
 
+    function calPrice(uint256 _image_id) view public returns(uint256) {
+        return(items[_image_id].price*(100+feePercent)/100);
+    }
 
-    // function transferNFT () {}
+    function purchaseNFT(uint256 _image_id) external payable nonReentrant {
+        require(_image_id > 0 && _image_id < itemCount, "Image not found.");
+        uint256 _price = calPrice(_image_id);
+        require(msg.value >= _price, "Insufficient balance for the buyer.");
+        GenshinImage storage image = items[_image_id];
+        require(!image.sold, "The image has already been sold.");
+        image.seller.transfer(image.price);
+        feeAccount.transfer(_price - image.price);
+        image.sold = true;
+        image.nft.transferFrom(address(this), msg.sender, image.token_id);
+        if (buyerstorage[_image_id] != address(123)) {
+            sold_or_not[image.image_url] = true;
+        }
+        buyerstorage[_image_id] = msg.sender;
+        buyersall[_image_id][msg.sender] = true;
+        sellerall[_image_id][image.seller] = true;
+        buyerpriceall[_image_id][msg.sender] = image.price;
+        sellerpriceall[_image_id][image.seller] = image.price;
+        sellertotalprice[_image_id][image.seller] = _price;
+        buyertotalprice[_image_id][msg.sender] = _price;
+        spentmoney[msg.sender] += _price;
+        earnmoney[image.seller] += image.price;
+        //Emit event.
+        emit Bought(
+            _image_id,
+            address(image.nft),
+            image.token_id,
+            image.price,
+            image.seller,
+            msg.sender,
+            image.most_sold
+        );
 
-    // function listNFTForSale () {}
+    }
 
+    function removeNFTFromSale(uint256 _image_id) external nonReentrant {
+        require(_image_id > 0 && _image_id < itemCount, "Image not found.");
+        require(items[_image_id].seller == msg.sender, "The user does not own this image.");
+        require(items[_image_id].sold == false, "The image has already been sold.");
+        GenshinImage storage image = items[_image_id];
+        if (buyerstorage[_image_id] == address(123)) {
+            buyerstorage[_image_id] = msg.sender;
+            items[_image_id].sold = true;
+        }
+        if(image.most_sold + 1 == originalmostsold[_image_id]){
+            appear_or_not[image.image_url] = false;
+            owner[image.image_url] = address(0);
+            left_sells[image.image_url] = 0;
+        }
+        else{
+            left_sells[image.image_url]++;
+            sold_or_not[image.image_url] = true;
+        }
+        image.nft.transferFrom(address(this), msg.sender, image.token_id);
+        withdrawornot[_image_id] = true;
+    }
 
-    // function removeNFTFromSale
+    function transferNFT(uint256 _image_id, address receiver) external nonReentrant {
+        require(_image_id > 0 && _image_id < itemCount, "Image not found.");
+        require(items[_image_id].sold == false, "The image has already been sold.");
+        require(buyerstorage[_image_id] == msg.sender, "The image is not owned by the sender");
+        require(receiver != address(0), "Recipient address cannot be zero.");
+        require(_isContract(receiver), "Recipient address is not a contract.");
+        
 
-    // function purchaseNFT
+        GenshinImage storage image = items[_image_id];
+        image.nft.transferFrom(msg.sender, receiver, image.token_id);
+    
+    }
+
+    function _isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
 
 }
