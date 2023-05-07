3cf9c4cd09d81132cf0651b6e92acba0483c6a34
Jingwei Zhang <daniel@JingweideMacBook-Pro.local>
2023-05-07 12:13:50 -0400

    add web

--- migrations/1_deploy_nft.js
+++ migrations/1_deploy_nft.js
@@ -1,6 +1,7 @@
 const market  = artifacts.require("Marketplace");
 const nft  = artifacts.require("NFTMint");
 
+
 module.exports = function(deployer) {
     let feePercent = 1;
     deployer.deploy(market, feePercent);
